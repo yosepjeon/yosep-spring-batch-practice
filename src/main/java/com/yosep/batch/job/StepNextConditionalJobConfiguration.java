835627bbeabac0f05cc5972d0d085a414e2c58a8
yoggaebi <enekelx1@gmail.com>
2020-09-09 16:30:57 +0900

    spring batch ch4 complete

--- src/main/java/com/yosep/batch/job/StepNextConditionalJobConfiguration.java
+++ src/main/java/com/yosep/batch/job/StepNextConditionalJobConfiguration.java
@@ -18,47 +18,52 @@ import lombok.extern.slf4j.Slf4j;
 public class StepNextConditionalJobConfiguration {
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
-	
-//	@Bean
-//	public Job stepNextConditionalJob() {
-//		return jobBuilderFactory.get("stepNextConditionalJob")
-//				.start(flow)
-//	}
-	
+
+	@Bean
+	public Job stepNextConditionalJob() {
+		return jobBuilderFactory.get("stepNextConditionalJob")
+				.start(conditionalJobStep1())
+					.on("FAILED") 				// FAILED 일 경우
+					.to(conditionalJobStep3()) 	// step3으로 이동한다.
+					.on("*") 					// step3의 결과와 관계 없이
+					.end() 						// step3으로 이동하면 Flow가 종료한다.
+				.from(conditionalJobStep1())	// step1으로 부터
+					.on("*")					// FAILED 외에 모든 경우
+					.to(conditionalJobStep2())	// step2로 이동한다.
+					.next(conditionalJobStep3())// step2가 정상 종료되면 step3으로 이동한다.
+					.on("*")					// step3의 결과 관계 없이
+					.end()						// step3으로 이동하면 Flow가 종료한다.
+				.end()							// Job 종료
+				.build();
+	}
+
 	@Bean
 	public Step conditionalJobStep1() {
-		return stepBuilderFactory.get("step1")
-				.tasklet((contribution, chunkContext) -> {
-					log.info(">>>>> This is stepNextConditionalJob Step1");
-					
-					/*
-					 	ExitStatus FAILED로 지정한다.
-					 	해당 status를 보고 flow가 진행된다.
-					 */
-					contribution.setExitStatus(ExitStatus.FAILED);
-					
-					return RepeatStatus.FINISHED;
-				})
-				.build();
+		return stepBuilderFactory.get("conditionalJobStep1").tasklet((contribution, chunkContext) -> {
+			log.info(">>>>> This is stepNextConditionalJob Step1");
+
+			/*
+			 * ExitStatus FAILED로 지정한다. 해당 status를 보고 flow가 진행된다.
+			 */
+			contribution.setExitStatus(ExitStatus.FAILED);
+
+			return RepeatStatus.FINISHED;
+		}).build();
 	}
-	
+
 	@Bean
 	public Step conditionalJobStep2() {
-		return stepBuilderFactory.get("conditionalJobStep2")
-				.tasklet((contribution, chunkContext) -> {
-					log.info(">>>>> This is stepNextConditionalJob Step2");
-					return RepeatStatus.FINISHED;
-				})
-				.build();
+		return stepBuilderFactory.get("conditionalJobStep2").tasklet((contribution, chunkContext) -> {
+			log.info(">>>>> This is stepNextConditionalJob Step2");
+			return RepeatStatus.FINISHED;
+		}).build();
 	}
-	
+
 	@Bean
 	public Step conditionalJobStep3() {
-		return stepBuilderFactory.get("conditionalJobStep3")
-				.tasklet((contribution, chunkContext) -> {
-					log.info(">>>>> This is stepNextConditionalJob Step3");
-					return RepeatStatus.FINISHED;
-				})
-				.build();
+		return stepBuilderFactory.get("conditionalJobStep3").tasklet((contribution, chunkContext) -> {
+			log.info(">>>>> This is stepNextConditionalJob Step3");
+			return RepeatStatus.FINISHED;
+		}).build();
 	}
 }
---
+++ src/main/java/com/yosep/batch/listener/SkipCheckingListener.java
@@ -0,0 +1,18 @@
+package com.yosep.batch.listener;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.listener.StepExecutionListenerSupport;
+
+public class SkipCheckingListener extends StepExecutionListenerSupport{
+	public ExitStatus afterStep(StepExecution stepExecution) {
+		String exitCode = stepExecution.getExitStatus().getExitCode();
+		
+		if(!exitCode.equals(ExitStatus.FAILED.getExitCode()) && 
+				stepExecution.getSkipCount() > 0) {
+			return new ExitStatus("COMPLETED WITH SKIPS");
+		}else {
+			return null;
+		}
+	}
+}
